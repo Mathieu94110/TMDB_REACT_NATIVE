65c460ace323ae91629e51c917c42e9209e44bf8
Mathieu Enault <me94110@gmail.com>
2020-08-29 18:03:46 +0200

    tmbd before introcing redux and add favorite animations

---
+++ API/TMDBApi.jsx
@@ -0,0 +1,17 @@
+const API_TOKEN = "94544faaa7071b0d32d511d38e9439ec";
+
+export function getFilmsFromApiWithSearchedText(text, page) {
+  const url =
+    "https://api.themoviedb.org/3/search/movie?api_key=" +
+    API_TOKEN +
+    "&language=fr&query=" +
+    text +
+    "&page=" +
+    page;
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
+export function getImageFromApi(name) {
+  return "https://image.tmdb.org/t/p/w300" + name;
+}
