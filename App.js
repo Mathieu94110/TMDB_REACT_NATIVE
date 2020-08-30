3a4cc9d050fadc5b0d06f0aabcc2d9db2fe7f05b
Mathieu Enault <me94110@gmail.com>
2020-08-30 17:06:18 +0200

    fix bug

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 // App.js
-
+import "react-native-gesture-handler";
 import React from "react";
-import Search from "./Components/Search";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+const Stack = createStackNavigator();
 
-export default class App extends React.Component {
-  render() {
-    return <Search />;
-  }
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import Search from "../Components/Search";
+export default function HomeScreen() {
+  return <Search />;
+}
