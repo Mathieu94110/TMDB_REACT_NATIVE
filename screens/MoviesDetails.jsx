9e3bd759195009ab928e47ad7ae83cf5f4f2e69d
Mathieu Enault <me94110@gmail.com>
2020-09-03 14:52:35 +0200

    changing moviesdetails component in class component and adding favorite icon

--- screens/MoviesDetails.jsx
+++ screens/MoviesDetails.jsx
@@ -2,81 +2,107 @@ import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
-  TextInput,
   Button,
   Text,
   FlatList,
   ActivityIndicator,
+  TouchableOpacity,
   Image,
 } from "react-native";
 import { getImageFromApi, getFilmDetailFromApi } from "../API/TMDBApi";
-import { BaseRouter } from "@react-navigation/native";
+
 import { connect } from "react-redux";
 
-function MoviesDetails(props) {
-  const filmId = props.route.params.filmId;
-  const [film, setFilm] = useState(null);
-  useEffect(() => {
+class MoviesDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      film: undefined,
+      isLoading: true,
+    };
+  }
+  componentDidMount() {
+    const filmId = this.props.route.params.filmId;
     getFilmDetailFromApi(filmId)
       .then((response) => response.json())
       .then((response) => {
-        setFilm(response);
+        this.setState({
+          film: response,
+          isLoading: false,
+        });
       })
       .catch((error) => console.error(error));
-  }, []);
-  console.log(props);
+  }
 
   componentDidUpdate = () => {
     console.log("componentDidUpdate : ");
-    console.log(props.favoritesFilm);
+    console.log(this.props.favoritesFilm);
   };
 
   //action
 
   _toggleFavorite = () => {
-    //on teste le changement pout rfc
-    const action = { type: "TOGGLE_FAVORITE", value: film };
-    props.dispatch(action); // voir si utile
+    const action = { type: "TOGGLE_FAVORITE", value: this.state.film };
+    this.props.dispatch(action);
   };
 
-  return (
-    <View style={styles.container}>
-      {film ? (
-        <View>
-          <Image
-            style={styles.image}
-            source={{ uri: getImageFromApi(film.poster_path) }}
-          />
+  _displayFavoriteImage() {
+    var sourceImage = require("./images/ic_favorite_border.png");
+    if (
+      this.props.favoritesFilm.findIndex(
+        (item) => item.id === this.state.film.id
+      ) !== -1
+    ) {
+      // Film dans nos favoris
+      sourceImage = require("./images/ic_favorite.png");
+    }
+    return <Image style={styles.favorite_image} source={sourceImage} />;
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.film ? (
           <View>
-            <Text>{film.title}</Text>
-          </View>
-          <Button title="Favoris" onPress={() => this._toggleFavorite()} />
-          <View>
-            <Text>{film.overview}</Text>
-          </View>
-          <View>
-            <Text>{film.release_date}</Text>
-          </View>
-          <View>
-            <Text>{film.vote_average}</Text>
-          </View>
-          <View>
-            <Text>{film.buget}</Text>
-          </View>
+            <Image
+              style={styles.image}
+              source={{ uri: getImageFromApi(this.state.film.poster_path) }}
+            />
+            <View>
+              <Text>{this.state.film.title}</Text>
+            </View>
 
-          <View>
-            {film.production_companies.map((company, index) => (
-              <Text key={index}>{company.name}</Text>
-            ))}
+            <TouchableOpacity
+              style={styles.logo_container}
+              onPress={() => this._toggleFavorite()}
+            >
+              {this._displayFavoriteImage()}
+            </TouchableOpacity>
+            <View>
+              <Text>{this.state.film.overview}</Text>
+            </View>
+            <View>
+              <Text>{this.state.film.release_date}</Text>
+            </View>
+            <View>
+              <Text>{this.state.film.vote_average}</Text>
+            </View>
+            <View>
+              <Text>{this.state.film.buget}</Text>
+            </View>
+            <View>
+              {this.state.film.production_companies.map((company, index) => (
+                <Text key={index}>{company.name}</Text>
+              ))}
+            </View>
           </View>
-        </View>
-      ) : (
-        <View>
-          <Text>Chargement en cours</Text>
-        </View>
-      )}
-    </View>
-  );
+        ) : (
+          <View>
+            <Text>Chargement en cours</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -88,6 +114,13 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 200,
+  },
+  logo_container: {
+    alignItems: "center",
+  },
+  logo: {
+    width: 40,
+    height: 40,
   },
 });
 
